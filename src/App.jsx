fe8d3771e05816691f6f714c20bc1281d5cece3b
m-faseeh <muhammadfaseeh397@gmail.com>
2024-10-23 22:19:22 +0500

    Portfolio project updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
-import Skills from './components/skills';
+import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
